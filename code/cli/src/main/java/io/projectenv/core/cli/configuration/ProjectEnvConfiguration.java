cc41b6d708035baeef1debae93cc23df0546c867
repolevedavaj <jonas.bamberger@gmail.com>
2021-11-04 20:22:07 +0100

    transformed code into commons library which can be used by a java Project-Env project

--- code/cli/src/main/java/io/projectenv/core/cli/configuration/ProjectEnvConfiguration.java
+++
@@ -1,9 +0,0 @@
-package io.projectenv.core.cli.configuration;
-
-import java.util.List;
-
-public interface ProjectEnvConfiguration extends ProjectEnvBaseConfiguration {
-
-    <T> List<T> getToolConfigurations(String toolName, Class<T> configurationClass);
-
-}
--- code/tool-support/tool-support-spi/src/main/java/io/projectenv/core/toolsupport/spi/ToolSupportException.java
+++
@@ -1,9 +0,0 @@
-package io.projectenv.core.toolsupport.spi;
-
-public class ToolSupportException extends Exception {
-
-    public ToolSupportException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
